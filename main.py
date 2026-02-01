93701ac413e32494ce7bd275c5e6bfd85aec8da4
Scott Lusk <221888325+scottluskraa@users.noreply.github.com>
2026-02-01 22:13:49 +0000

    add instructions

--- main.py
+++ main.py
@@ -1,12 +1,72 @@
 """
-Main module for Python starter template.
-This is a simple Hello World placeholder to get started.
+Main module for Python assignment.
+This assignment demonstrates use of Variables and Conditional Statements.
 """
 
 
-def main():
-    """Print a hello world message."""
-    print("Hello, World!")
+def main(): 
+    # here for display purposes
+    print("=" * 75)
+
+    # ------------------------------------------------------------
+    # STEP 1: 
+    #   Create variables
+    #
+    # Instructions:
+    #   You will need 3 variables to help you with this assignment
+    #   placeholders are below indicating the variable and it's 
+    #   data type that you should use.
+    # ------------------------------------------------------------
+
+    # store the member's name (string)  
+
+    # store the number of books currently check out (number)
+
+    # store whether the library account is active (boolean)
+
+
+    # ------------------------------------------------------------
+    # STEP 2: 
+    #   Print the member's information
+    # 
+    # Instructions:
+    #   We want to PRINT the information for the member. You can 
+    #   do this using the Python print function, see example below.
+    #
+    # Example:
+    #   name = "Bob"
+    #   print("Name:", name)
+    #
+    # ------------------------------------------------------------
+
+    # print the member's name
+
+    # print the number of books currently checked out
+
+    # print whether the library account is active
+
+
+    # ------------------------------------------------------------
+    # STEP 3: 
+    #   Use conditional statements to check library rules
+    # 
+    # Instructions:
+    #   Write conditional statements that check the library rules
+    #     Rules:
+    #       - The account must be active
+    #       - A member can check out up to 5 books
+    # 
+    #   The expected result is PRINT using the print function, 
+    #   to indicate if the member can checkout any more books
+    #
+    # ------------------------------------------------------------
+
+    # conditional statement goes here to check library rules
+    # You must use an if, elif, and else conditional statement
+
+    # ------------------------------------------------------------
+    # here for display purposes
+    print("=" * 75)
 
 
 if __name__ == "__main__":
